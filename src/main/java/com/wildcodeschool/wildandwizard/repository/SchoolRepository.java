fd18754f5ee498e6b2d3516465b3ec7722ade6fe
Arsene Ella <aellalongin@gmai.com>
2019-12-16 14:26:15 +0100

    quest quest-spring-jpa-hibernate DONE

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,6 +1,12 @@
 package com.wildcodeschool.wildandwizard.repository;
 
-// TODO : update this interface
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface SchoolRepository {
+import com.wildcodeschool.wildandwizard.entity.School;
+
+// TODO : update this interface
+@Repository
+public interface SchoolRepository extends JpaRepository<School, Long> {
 }
+
